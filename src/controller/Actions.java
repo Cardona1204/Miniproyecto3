f454eb0e37409e48661aa628a0b2c79472ebcadd
Manuel Cardona <107329770+Cardona1204@users.noreply.github.com>
2023-11-30 20:43:23 -0500

    Add files via upload

---
+++ src/controller/Actions.java
@@ -0,0 +1,18 @@
+package controller;
+
+public enum Actions {
+    INSERT,
+    UPDATE,
+    DELETE,
+    SEARCH,
+    LIST,
+    WINNER,
+    ADD_PROPOSALS,
+    ADD_VOTES,
+    ABOUT,
+    HELP,
+    CITY_WITH_LESS_CANDIDATES,
+    PARTY_WITH_CANDIDATES,
+    WINNER_PROPOSALS,
+    EXIT
+}
